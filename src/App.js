a95f4b24d68cd922374282a517ba145b57004f21
shadan72 <shadanabdulkarim94@gmail.com>
2020-07-27 20:26:22 +0300

    adding tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './styles/main.css';
 
 function App() {
   return (
-    <div>
-      <h1>The Movie Bubble</h1>
-    </div>
+    <div class=" flex items-center justify-center h-screen ">
+    <div class="bg-pink-600 bg-opacity-50 hover:bg-blue-400 w-64 h-10  items-center cursor-pointer font-bold">
+      <h1 class="text-red">The Movie Bubble</h1>
+    </div> 
+  </div>
   );
 }
 
