c614b58093132b254ca60768b2ff3dae2b34445a
shadan72 <shadanabdulkarim94@gmail.com>
2020-07-28 14:19:42 +0300

    Fixing with lint and fix commands

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './styles/main.css';
 
 function App() {
   return (
-    <div class=" flex items-center justify-center h-screen ">
-    <div class="bg-pink-600 bg-opacity-50 hover:bg-blue-400 w-64 h-10  items-center cursor-pointer font-bold">
-      <h1 class="text-red">The Movie Bubble</h1>
-    </div> 
-  </div>
+    <div className=" flex items-center justify-center h-screen ">
+      <div className="bg-pink-600 bg-opacity-50 hover:bg-blue-400 w-64 h-10  items-center cursor-pointer font-bold">
+        <h1 className="text-red">The Movie Bubble</h1>
+      </div>
+    </div>
   );
 }
 
