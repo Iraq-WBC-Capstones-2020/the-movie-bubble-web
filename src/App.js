bd734a421824a31981d396f41dbc2af364fa153f
Abdulrahman Maher <ibn.maher.96@gmail.com>
2020-07-29 17:16:21 +0300

    sliders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Sliders from './components/Sliders/Sliders';
 function App() {
-  return( <>
-  <h1>the movie bubble</h1>;
-  <Sliders />
-  </>
-  )
-
+  return (
+    <>
+      <h1>the movie bubble</h1>;
+      <Sliders />
+    </>
+  );
 }
 
 export default App;
