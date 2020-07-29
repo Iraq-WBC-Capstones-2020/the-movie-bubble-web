79f38b6b40ddf70187c8b0269160521b3b5da009
Revan Sarbast Salih <revansarbast8@gmail.com>
2020-07-29 23:49:05 +0300

    Revan99/issue2 Refactor: BEM naming
    implementing BEM naming for css class
    animation added

--- src/components/AutorizationButton/AuthorizationButton.js
+++ src/components/AutorizationButton/AuthorizationButton.js
@@ -4,22 +4,31 @@ import { FaFacebookF } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
 export default function AuthorizationButton() {
   return (
-    <div className="wrapper">
-      <div className="registerLoginContainer">
+    <div className="joinUsWrapper">
+      <div className="joinUsWrapper__registerLoginWrapper">
         <button className="login">
           <h3>Login</h3>
         </button>
 
-        <button className="register">
+        <button
+          className="
+        joinUsWrapper__registerLoginWrapper__register 
+        "
+        >
           <h3>Register</h3>
         </button>
       </div>
-      <div className="continueWithContainer">
+      <div
+        className="
+        continueWithWrapper
+        "
+      >
         <div className="continueWith">
           <div>
             <h3 className="continueWithText">Continue With</h3>
           </div>
         </div>
+
         <button className="faceBook">
           <FaFacebookF className="faceBool__logo"></FaFacebookF>
         </button>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        lightblue: '#18A0FB', // an example of extending tailwind
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
