a5679041cd28783c9f165deab4bebcfca92e8045
Revan Sarbast Salih <revansarbast8@gmail.com>
2020-07-30 13:05:46 +0300

    commenting jsx

--- src/components/AutorizationButton/AuthorizationButton.js
+++ src/components/AutorizationButton/AuthorizationButton.js
@@ -4,36 +4,72 @@ import { FcGoogle } from 'react-icons/fc';
 export default function AuthorizationButton() {
   return (
     <>
+      {/* this div wrap the whole component */}
       <div className="joinUsWrapper">
+        {/* this div wrap register and login button*/}
         <div className="joinUsWrapper__registerLoginWrapper">
-          <button className="joinUsWrapper__registerLoginWrapper__login">
+          <button
+            className="
+            joinUsWrapper__registerLoginWrapper__login
+            login-registerButton
+            "
+          >
             <h3>Login</h3>
           </button>
 
           <button
             className="
-        joinUsWrapper__registerLoginWrapper__register 
-        "
+            joinUsWrapper__registerLoginWrapper__register 
+            login-registerButton
+            "
           >
             <h3>Register</h3>
           </button>
         </div>
+        {/* this div wrap continue with button and divs*/}
         <div
           className="
-        joinUsWrapper__continueWithWrapper
-        "
+          joinUsWrapper__continueWithWrapper
+          "
         >
-          <div className="joinUsWrapper__continueWithWrapper__continueWith">
+          {/* this div is for continue with part*/}
+          <div
+            className="
+            joinUsWrapper__continueWithWrapper__continueWith
+            "
+          >
+            {/* this div contain the text and show linearw boarder*/}
             <div>
-              <h3 className="continueWithText">Continue With</h3>
+              <h3>Continue With</h3>
             </div>
           </div>
-
-          <button className="joinUsWrapper__faceBook">
-            <FaFacebookF className="joinUsWrapper__faceBook__faceBook__logo"></FaFacebookF>
+          {/* facebook button */}
+          <button
+            className="
+            joinUsWrapper__facebook
+            facebook-google
+            "
+          >
+            {/* facebook logo */}
+            <FaFacebookF
+              className="
+              joinUsWrapper__facebook__facebook__logo
+              "
+            ></FaFacebookF>
           </button>
-          <button className="joinUsWrapper__google">
-            <FcGoogle className="joinUsWrapper__google__google__logo"></FcGoogle>
+          {/* google button  */}
+          <button
+            className="
+            joinUsWrapper__google
+            facebook-google
+            "
+          >
+            {/* google logo */}
+            <FcGoogle
+              className="
+              joinUsWrapper__google__google__logo
+              "
+            ></FcGoogle>
           </button>
         </div>
       </div>
