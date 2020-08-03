2a18fe03953e1fc0bbceef145117be20941a9b6b
shadan72 <shadanabdulkarim94@gmail.com>
2020-08-03 16:48:52 +0300

    create the BubbleButton component Fixes #3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import BubbleButton from './components/BubbleButton';
 function App() {
   return (
     <div>
       <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
+      <BubbleButton />
     </div>
   );
 }
