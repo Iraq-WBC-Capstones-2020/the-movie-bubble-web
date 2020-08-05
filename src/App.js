ed10751252cd7c892653e94a5a48f6201049dda1
Abdulrahman Maher <ibn.maher.96@gmail.com>
2020-08-05 06:38:55 +0300

    changed some classes name and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Sliders from './components/Sliders/Sliders';
+import Login from './components/Login/Login';
+import './App.css';
 function App() {
   return (
     <>
-      <h1>the movie bubble</h1>;
+  <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
       <Sliders />
     </>
   );
