525c497517918c09dfe83fba485326514a512ed3
shadan72 <shadanabdulkarim94@gmail.com>
2020-08-07 19:00:50 +0300

    Build movie card component includes movie card and movie grid Fixes #23

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import MovieGrid from './components/MovieGrid/MovieGrid';
 function App() {
   return (
-    <div>
+    <div className="dark-mode-background">
       <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
+      <MovieGrid />
     </div>
   );
 }
---
+++ src/components/MovieGrid/MovieGrid.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import MovieCard from '../../components/MovieCard/MovieCard';
+import './MovieGrid.css';
+export default function MovieGrid() {
+  const movieData = [
+    {
+      id: '0',
+      Title: 'Birds of Pray',
+      poster_path:
+        'https://image.tmdb.org/t/p/w600_and_h900_bestv2/h4VB6m0RwcicVEZvzftYZyKXs6K.jpg',
+    },
+    {
+      id: '1',
+      Title: 'Harry Porter',
+      poster_path:
+        'https://image.tmdb.org/t/p/w600_and_h900_bestv2/csOv5H7R2zdnKaYuTrGVWohmo8d.jpg',
+    },
+    {
+      id: '2',
+      Title: 'Scoob! (2020)',
+      poster_path:
+        'https://image.tmdb.org/t/p/w600_and_h900_bestv2/zG2l9Svw4PTldWJAzC171Y3d6G8.jpg',
+    },
+    {
+      id: '3',
+      Title: 'Star Wars',
+      poster_path:
+        'https://image.tmdb.org/t/p/w600_and_h900_bestv2/db32LaOibwEliAmSL2jjDF6oDdj.jpg',
+    },
+    {
+      id: '4',
+      Title: 'Avengers',
+      poster_path:
+        'https://image.tmdb.org/t/p/w600_and_h900_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg',
+    },
+    {
+      id: '5',
+      Title: 'Interstellar',
+      poster_path:
+        'https://image.tmdb.org/t/p/w600_and_h900_bestv2/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg',
+    },
+    {
+      id: '6',
+      Title: 'An American Pickle',
+      poster_path:
+        'https://image.tmdb.org/t/p/w600_and_h900_bestv2/4HEg1qXxGXQk1ds2A3gCnJpuP2z.jpg',
+    },
+  ];
+  return (
+    <div className="grid justify-center">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+        {movieData.map((movieItem, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-center movie-grid-card  mb-4"
+          >
+            <MovieCard singleMovie={movieItem} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
