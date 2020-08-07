9d3178330c2c71dfd6a5af07a670026855b1ad18
Abdulrahman Maher <ibn.maher.96@gmail.com>
2020-08-07 04:07:01 +0300

    movie projector component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import MovieProjector from './components/MovieProjector/MovieProjector';
 function App() {
   return (
-    <div>
+    <div className="bg-gray-900">
       <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
+      <MovieProjector />
     </div>
   );
 }
