5cfdec28c5118395f571022d08fcfb7d1d4d9920
Hewr Srood <63727790+Hewr-Srood@users.noreply.github.com>
2020-08-08 04:21:37 +0300

    Fixes issue#7 done requested changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import SidebarNav from './components/HamburgerMenu/SidebarNav';
 
 function App() {
   return (
     <div>
-      <SidebarNav />
       <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
     </div>
   );
