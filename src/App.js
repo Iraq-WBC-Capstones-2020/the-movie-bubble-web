bee49cd11140afca061dbbea070bf4b0869205f4
Hewr Srood <63727790+Hewr-Srood@users.noreply.github.com>
2020-08-08 08:11:16 +0300

    tests passed successfully

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Quiz from './pages/Quiz';
@@ -9,21 +9,23 @@ import Error404 from './pages/Error/Error404';
 function App() {
   return (
     <>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/quiz">
-          <Quiz />
-        </Route>
-        <Route path="*" exact={true} component={Error404} />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/quiz">
+            <Quiz />
+          </Route>
+          <Route path="*" exact={true} component={Error404} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
