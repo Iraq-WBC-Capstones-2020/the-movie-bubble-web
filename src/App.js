c121544a9e9786c574d3bc3215110bba4c3bd4c2
Wisam Naji <guitar90rocker@gmail.com>
2020-08-10 20:31:12 +0300

    reset app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import MovieProjector from './components/MovieProjector/MovieProjector';
 
 function App() {
   return (
-    <div className="bg-gray-900">
+    <div>
       <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
-      <MovieProjector />
     </div>
   );
 }
