53e2e90800f4a831f220ba25bcf1dd5825b17209
Revan Sarbast Salih <revansarbast8@gmail.com>
2020-08-10 14:53:30 +0300

    create questions component Fixes #26

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: false,
   theme: {
     extend: {
+      backgroundColor: (theme) => ({
+        ' linearorange':
+          'linear-gradient(270deg, #FF7521 11.7%, #FFB421 90.81%)',
+      }),
       colors: {
         lightblue: '#18A0FB', // an example of extending tailwind
         darkgray: '#1D2530',
