c91e1f4dafac4f358c7694a03f84ec27a1e65f17
Abdulrahman Maher <ibn.maher.96@gmail.com>
2020-08-10 17:35:50 +0300

    single moive page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import MoviePage from './pages/MoviePage/MoviePage';
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
-    </div>
+    <>
+      <div>
+        <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
+      </div>
+      <MoviePage />
+    </>
   );
 }
 
