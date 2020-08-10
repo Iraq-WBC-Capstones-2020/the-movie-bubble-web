89662ba9a576d8054b14b4333b4a1f9adb4d776a
Wisam Naji <guitar90rocker@gmail.com>
2020-08-10 19:46:38 +0300

    reverted files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import BubbleButton from './components/BubbleButton';
 function App() {
   return (
     <div>
       <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
-      <BubbleButton />
     </div>
   );
 }
