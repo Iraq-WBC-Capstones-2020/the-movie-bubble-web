08109c5c019ea70c2c423157f1cbf0f3a0a0e9f0
Revan Sarbast Salih <revansarbast8@gmail.com>
2020-08-11 14:01:17 +0300

    create join-us component Fixes #2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import AuthorizationButton from './components/AutorizationButton/AuthorizationButton';
 
 function App() {
   return (
     <div>
-      <AuthorizationButton />
       <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
     </div>
   );
