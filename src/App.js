0354921b11dd181db5e9ca0b0b61f9aa2782d8e7
Revan Sarbast Salih <revansarbast8@gmail.com>
2020-08-14 11:09:53 +0300

    create questions component Fixes #26

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
