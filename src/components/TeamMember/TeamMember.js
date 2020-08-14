54d4d617cf08eafdfbb1b378f307ade42622df01
osamaakb <osama98akb@gmail.com>
2020-08-14 13:40:47 +0300

    Style: remove about page unnecessary classes

--- src/components/TeamMember/TeamMember.js
+++ src/components/TeamMember/TeamMember.js
@@ -5,39 +5,35 @@ import '../TeamMember/TeamMember.css';
 export default function TeamMember({ isRight, name, role, image }) {
   if (isRight)
     return (
-      <>
-        <div className="teammember  w-full  flex overflow-hidden my-10">
-          <div className="member-img h-full  " data-aos="fade-right">
-            <img
-              className="w-full lg:w-6/12 sm:w-full"
-              alt=""
-              src={image}
-            ></img>
-          </div>
-          <div
-            className="member-caption  flex flex-col justify-center items-center "
-            data-aos="fade-left"
-          >
-            <h1 className="member-name text-white ">{name}</h1>
-            <h2 className="member-role text-white ">{role}</h2>
-          </div>
-        </div>
-      </>
-    );
-  return (
-    <>
-      <div className="teammember w-full  flex flex-row-reverse overflow-hidden">
-        <div className="member-img-left  h-full " data-aos="fade-left">
-          <img className="bg-cover " alt="team member" src={image}></img>
+      <div className="teammember w-full flex overflow-hidden mt-10">
+        <div className="member-img h-full" data-aos="fade-right">
+          <img
+            className="w-full lg:w-6/12 sm:w-full"
+            alt=""
+            src={image}
+          ></img>
         </div>
         <div
-          className="member-caption-left flex flex-col justify-center items-center  "
-          data-aos="fade-right"
+          className="member-caption flex flex-col justify-center items-center"
+          data-aos="fade-left"
         >
-          <h1 className="text-white member-name">{name}</h1>
-          <h2 className="text-white member-role ">{role}</h2>
+          <h1 className="member-name text-white">{name}</h1>
+          <h2 className="member-role text-white">{role}</h2>
         </div>
       </div>
-    </>
+    );
+  return (
+    <div className="teammember w-full  flex flex-row-reverse overflow-hidden mt-10">
+      <div className="member-img-left  h-full" data-aos="fade-left">
+        <img className="bg-cover" alt="team member" src={image}></img>
+      </div>
+      <div
+        className="member-caption-left flex flex-col justify-center items-center"
+        data-aos="fade-right"
+      >
+        <h1 className="text-white member-name">{name}</h1>
+        <h2 className="text-white member-role">{role}</h2>
+      </div>
+    </div>
   );
 }
--- src/pages/Aboutpage/Aboutpage.js
+++ src/pages/Aboutpage/Aboutpage.js
@@ -16,38 +16,31 @@ const Aboutpage = () => {
     });
   });
   return (
-    <>
-      <div className="about-content">
-        <div
-          className="aboutpage  w-full py-24 px-64 relative text-base
-            overflow-hidden bg-gray-700 h-64 "
-          data-aos="zoom-out"
-        >
-          <p className="aboutpage-text absolute">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
+    <div className="about-content">
+      <div
+        className="aboutpage w-full h-64 flex justify-center"
+        data-aos="zoom-out"
+      >
+        <div className="text-3xl flex flex-col justify-center items-center h-full">
+          <p className="font-bold text-center">Don't get lost in the internet looking for a movie to watch, with us you can find it in a minute.</p>
+          <p className="text-xl opacity-75 pt-5">Built with love ❤️</p>
         </div>
-
-        <TeamMember isRight name="Revan" role="Team Member" image={Img1} />
-        <TeamMember name="Randa Yahya" role="Team Member" image={Img2} />
-        <TeamMember isRight name="Hewr" role="Team Member" image={Img3} />
-        <TeamMember name="shadan" role="Team Member" image={Img4} />
-        <TeamMember
-          isRight
-          name="Abdulrahman"
-          role="Team Member"
-          image={Img5}
-        />
       </div>
-    </>
+
+      <TeamMember isRight name="Revan" role="Team Member" image={Img1} />
+      <TeamMember name="Randa Yahya" role="Team Member" image={Img2} />
+      <TeamMember isRight name="Hewr" role="Team Member" image={Img3} />
+      <TeamMember name="shadan" role="Team Member" image={Img4} />
+      <TeamMember
+        isRight
+        name="Abdulrahman"
+        role="Team Member"
+        image={Img5}
+      />
+      <div className="h-24 flex justify-center items-center">
+        <p className="text-white bg-orange-600 px-4 py-2">All rights reserved © The Movie Bubble 2020</p>
+      </div>
+    </div>
   );
 };
 export default Aboutpage;
