696ab3d0970c602ecf2202c4441d4028f62dfac7
osamaakb <osama98akb@gmail.com>
2020-08-18 12:28:00 +0300

    Fix: tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,35 +6,8 @@ module.exports = {
         lightblue: '#18A0FB', // an example of extending tailwind
         darkgray: '#1D2530',
       },
-      borderRadius: {
-        20: '20px',
-      },
-      boxShadow: {
-        '3xl': '28px 2px 18px 10px #101010',
-      },
-      fontSize: {
-        '15r': '15rem',
-        '10r': '10rem',
-      },
       spacing: {
         '0.08': '0.08rem',
-        '-20r': '-20rem',
-        '-10': '-10rem',
-      },
-      width: {
-        '300p': '300px',
-        '40%': '40%',
-        '60r': '60rem',
-      },
-      minHeight: {
-        '10': '2.5rem',
-      },
-      minWidth: {
-        '20': '7rem',
-      },
-      inset: {
-        '10%': '10%',
-        '60vh': '60vh',
       },
     },
     screens: {
@@ -43,15 +16,13 @@ module.exports = {
       md: { min: '701px', max: '970px' },
       lg: { min: '971px', max: '1300px' },
       xl: { min: '1301px' },
-      'max-910p': { max: '910px' },
+    },
+    skew: {
+      '-20': '-20deg',
+
     },
   },
-
-  variants: {
-    scale: ['hover'],
-    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
-  },
-
+  variants: { scale: ['hover'] },
   plugins: [
     function ({ addComponents }) {
       const buttons = {
@@ -59,7 +30,6 @@ module.exports = {
           background: 'linear-gradient(270deg, #ffb421 11.7%, #ff7521 90.81%)',
         },
       };
-
       addComponents(buttons);
     },
   ],
