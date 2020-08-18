512a0e473f19408bdfbafc24dc988fd471821ed2
osamaakb <osama98akb@gmail.com>
2020-08-18 11:40:28 +0300

    Fix: remove question test

--- src/components/__test__/Questions.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '@testing-library/jest-dom/extend-expect';
-import Questions from '../Questions/Questions';
-
-it('render without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Questions />, div);
-});
