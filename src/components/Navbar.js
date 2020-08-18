e3223dd8fab84e7f6c647467d77fe3e758598cfe
Hewr Srood <63727790+Hewr-Srood@users.noreply.github.com>
2020-08-18 13:19:53 +0300

    create Navbar Component Fixes #58

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Menu from './HamburgerMenu/SidebarNav'
+import Logo from './Logo/Logo'
+export default function Navbar() {
+    return (
+        <div>
+            <Menu></Menu>
+            <Logo/>
+        </div>
+    )
+}
