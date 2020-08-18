d88a4a149a05a4a46a1a5d51f930f15a2d0ca2ae
Hewr Srood <63727790+Hewr-Srood@users.noreply.github.com>
2020-08-18 13:22:21 +0300

    ran yarn fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Menu from './HamburgerMenu/SidebarNav'
-import Logo from './Logo/Logo'
+import React from 'react';
+import Menu from './HamburgerMenu/SidebarNav';
+import Logo from './Logo/Logo';
 export default function Navbar() {
-    return (
-        <div>
-            <Menu></Menu>
-            <Logo/>
-        </div>
-    )
+  return (
+    <div>
+      <Menu></Menu>
+      <Logo />
+    </div>
+  );
 }
--- src/components/Sliders/Sliders.js
+++ src/components/Sliders/Sliders.js
@@ -9,68 +9,84 @@ const Sliders = () => {
     <>
       <div className="slider__container hidden lg:flex flex-row absolute w-1/2 h-full">
         <motion.div
-           animate={{
-            x: 79
+          animate={{
+            x: 79,
           }}
           initial={{
-            x: 0
+            x: 0,
           }}
-          transition={{ duration: 1, ease:"easeInOut", delay: 1}}
+          transition={{ duration: 1, ease: 'easeInOut', delay: 1 }}
           className="   first z-40 mt-4"
         >
           <div className="shadow shadow-3xl rounded-20 bg-gray-400">
-            <img src={First} alt="poster-1" className="poster rounded-20 opacity-75"></img>
+            <img
+              src={First}
+              alt="poster-1"
+              className="poster rounded-20 opacity-75"
+            ></img>
           </div>
         </motion.div>
         <motion.div
-           animate={{
+          animate={{
             x: 79,
             y: 16,
             rotate: 6,
           }}
           initial={{
             x: -150,
-            y:14
+            y: 14,
           }}
-          transition={{ duration: 1, ease:"easeInOut",  delay: 1}}
+          transition={{ duration: 1, ease: 'easeInOut', delay: 1 }}
           className="second z-30 -ml-48"
         >
           <div className="shadow shadow-3xl rounded-20">
-            <img src={Second} alt="poster-2" className="poster rounded-20"></img>
+            <img
+              src={Second}
+              alt="poster-2"
+              className="poster rounded-20"
+            ></img>
           </div>
         </motion.div>
         <motion.div
-           animate={{
+          animate={{
             x: 79,
             y: 38,
             rotate: 16,
           }}
           initial={{
             x: -270,
-            y:15
+            y: 15,
           }}
-          transition={{ duration: 1, ease:"easeInOut", delay: 1}}
+          transition={{ duration: 1, ease: 'easeInOut', delay: 1 }}
           className="third z-20 -ml-56"
         >
           <div className="shadow shadow-3xl rounded-20">
-            <img src={Third} alt="poster-3" className="poster rounded-20 "></img>
+            <img
+              src={Third}
+              alt="poster-3"
+              className="poster rounded-20 "
+            ></img>
           </div>
         </motion.div>
         <motion.div
-           animate={{
+          animate={{
             x: 79,
             y: 68,
             rotate: 26,
           }}
           initial={{
             x: -320,
-            y:15
+            y: 15,
           }}
-          transition={{ duration: 1,ease:"easeInOut",  delay: 1 }}
+          transition={{ duration: 1, ease: 'easeInOut', delay: 1 }}
           className="fourth z-10 -ml-64"
         >
           <div className="shadow shadow-3xl rounded-20">
-            <img src={Fourth} alt="poster-4" className="poster rounded-20"></img>
+            <img
+              src={Fourth}
+              alt="poster-4"
+              className="poster rounded-20"
+            ></img>
           </div>
         </motion.div>
       </div>
