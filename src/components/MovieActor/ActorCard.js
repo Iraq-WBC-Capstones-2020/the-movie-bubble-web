8d39c03bb9fa73ee834c954813ac1bcc39fd1dc6
osamaakb <osama98akb@gmail.com>
2020-08-18 12:14:51 +0300

    Style: add alt to actor img

--- src/components/MovieActor/ActorCard.js
+++ src/components/MovieActor/ActorCard.js
@@ -5,9 +5,9 @@ export default function ActorCard({ actor }) {
   return (
     <>
       <img
-        className="  transform skew-x-6 object-cover w-32 -ml-3 h-48 absolute overflow-hidden max-w-none"
+        className="transform skew-x-6 object-cover w-32 -ml-3 h-48 overflow-hidden max-w-none"
         src={actor.actor_image}
-        alt=""
+        alt="actor"
       />
       <div className="actor-card__name absolute inset-x-0 bottom-0 h-12 bg-gray-100 hidden">
         <h1 className=" text-center text-orange-600 font-bold ">
