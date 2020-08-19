73bdde6314500c0185ed187d6d5ab008149b69ec
osamaakb <osama98akb@gmail.com>
2020-08-19 12:52:24 +0300

    Chore: move firewaves to quiz

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import FireWave from '../components/FireWaves/FireWave';
 export default function Home() {
   return (
     <div>
-      <FireWave />
       <h1 className="text-2xl bg-lightblue">The Movie Bubble</h1>
     </div>
   );
