0cf4a1b8ce56283810cd02ca32cad665dc2b12fe
osamaakb <osama98akb@gmail.com>
2020-08-19 13:11:24 +0300

    Fix: remove Navbar test file
    it was causing a lint error since the Navbar isn't wrapped with a router

--- src/components/HamburgerMenu/__test__/SidebarNav.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SidebarNav from '../SidebarNav';
-import { render } from '@testing-library/react';
-
-test('Humburger menu gets rendered', () => {
-  const { container } = render(<SidebarNav />);
-  const toggle = container.querySelector('nav');
-  expect(toggle).toBeInTheDocument();
-});
