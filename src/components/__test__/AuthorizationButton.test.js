2aec59975f270f22f0d00e30b9c351b9412f81c1
shadan72 <shadanabdulkarim94@gmail.com>
2020-08-20 20:54:53 +0300

    Home Page Refactor : home page and its components Fixes #52

--- src/components/__test__/AuthorizationButton.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AuthorizationButton from '../AuthorizationButton';
-import '@testing-library/jest-dom/extend-expect';
-
-it('render without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AuthorizationButton />, div);
-});
